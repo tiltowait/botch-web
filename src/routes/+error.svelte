0605eac3a5b2dc278a9a7d5f68d843d3b0411f96
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-10-26 11:39:46 -0700

    Better error messages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { page } from '$app/stores'
+</script>
+
+<div class="block card variant-ghost-error p-4">
+  <header class="card-header">
+    <h1 class="h1 mb-2">Oops</h1>
+    <h5 class="h5 mb-6">Something went wrong.</h5>
+  </header>
+  <section>{$page.error?.message}</section>
+</div>
---
+++ src/routes/wizard/+page.svelte
@@ -0,0 +1,7 @@
+<div class="block card variant-ghost-error p-4">
+  <header class="card-header">
+    <h1 class="h1 mb-2">Oops</h1>
+    <h5 class="h5 mb-6">You probably got here by accident.</h5>
+  </header>
+  <section>If you want to create a character, run <code>/character wizard</code> in Discord.</section>
+</div>
