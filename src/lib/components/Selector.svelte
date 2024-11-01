39599ddf485ffa7a0cdaf0d83b15b4019088d89e
tiltowait <208040+tiltowait@users.noreply.github.com>
2024-11-01 16:33:49 -0700

    Fix chevron

--- src/lib/components/Selector.svelte
+++ src/lib/components/Selector.svelte
@@ -40,7 +40,7 @@
       <option value={option}>{option}</option>
     {/each}
   </select>
-  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-400">
+    <svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
   </div>
 </div>
